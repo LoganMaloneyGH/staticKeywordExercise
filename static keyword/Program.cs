dc1e13eaed238f339ca5b39e516d0bb36a075004
logan Maloney <shakenasty13@gmail.com>
2021-12-06 09:51:04 -0600

    initial commit

---
+++ static keyword/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is the temperature in F you want to convert?");
+            double fahTemp = Convert.ToDouble(Console.ReadLine());
+            double ConvertToCel = TempConverter.FahrenheitToCelsius(fahTemp);
+
+            Console.WriteLine($"that equals {ConvertToCel} degrees Celsius.\n");
+
+            Console.WriteLine("what is the temperature in celsius you want to convert?");
+            double celTemp = Convert.ToDouble(Console.ReadLine()); ;
+            double convertToFah = TempConverter.CelsiusToFahrenheit(celTemp);
+
+            Console.WriteLine($"that equals {convertToFah} degrees Fahrenheit."); 
+        }
+    }
+}
---
+++ static keyword/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_keyword
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            var celTemp = (fTemp - 32) * 5 / 9;
+            return celTemp; 
+        }
+
+        public static double CelsiusToFahrenheit(double cTemp)
+        {
+            var fahTemp = (cTemp * 9 / 5) + 32;
+            return fahTemp; 
+        }
+    }
+}
